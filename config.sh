32408907f7e5fb3d184ff97f8668d267aef9881e
Stafford Horne <shorne@gmail.com>
2017-12-03 21:17:40 +0900

    Bump versions to gcc 7.2.0, binutils 2.30, musl 1.1.19
    Thanks rth

--- config.sh
+++ config.sh
@@ -1,14 +1,22 @@
-BINUTILS_VERSION=2.26
-GCC_VERSION=5.4.0
-MUSL_VERSION=1.1.16
-LINUX_HEADERS_VERSION=4.5
+BINUTILS_VERSION=2.30
+GCC_VERSION=7.2.0
+MUSL_VERSION=1.1.19
+LINUX_HEADERS_VERSION=4.15
 
 ARCH=or1k
-BINUTILS_URL=http://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.bz2
 
-GCC_URL=https://github.com/openrisc/or1k-gcc/archive/musl-${GCC_VERSION}.tar.gz
+#BINUTILS_URL=http://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.bz2
+# Using openrisc binutils for rth's reloc work, switch back to gnu after
+# upstream
+BINUTILS_URL=https://github.com/openrisc/binutils-gdb/archive/or1k-${BINUTILS_VERSION}.tar.gz
 
-GCC_EXTRACT_DIR=or1k-gcc-musl-${GCC_VERSION}
+# Using our openrisc port until we have one upstream
+GCC_URL=https://github.com/openrisc/or1k-gcc/archive/or1k-${GCC_VERSION}.tar.gz
+
+BINUTILS_EXTRACT_DIR=binutils-gdb-or1k-${BINUTILS_VERSION}
+BINUTILS_VERSION=or1k-${BINUTILS_VERSION}
+
+GCC_EXTRACT_DIR=or1k-gcc-or1k-${GCC_VERSION}
 GCC_VERSION=or1k-${GCC_VERSION}
 GCC_BUILTIN_PREREQS=yes
 
